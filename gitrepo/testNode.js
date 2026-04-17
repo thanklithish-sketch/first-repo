c3f68834184e553a6626e5c2cda1f1532ea2c789
thanklithish-sketch <thanklithish@gmail.com>
2026-04-17 13:19:49 +0530

    git first code

--- gitrepo/testNode.js
+++ gitrepo/testNode.js
@@ -1,1 +1,11 @@
+function sum(a, b) {
+  return a + b;
+}
 
+// simple test (no jest needed)
+if (sum(1, 2) !== 3) {
+  console.error("Test failed");
+  process.exit(1);
+}
+
+console.log("Test passed");
